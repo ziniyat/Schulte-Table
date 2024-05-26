0e8b5ab6c7c5a52ef7d539c39efc8d86a4067df0
koshkilda <89313188+koshkilda@users.noreply.github.com>
2024-05-27 02:00:46 +0300

    Add files via upload

---
+++ SchulteTable/src/main/java/com/example/schultetable/TableController.java
@@ -0,0 +1,99 @@
+package com.example.schultetable;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TableController {
+
+    private String[] buttons;
+
+    @FXML
+    private Button mode4x4;
+
+    @FXML private Button b1;
+    @FXML private Button b2;
+    @FXML private Button b3;
+    @FXML private Button b4;
+    @FXML private Button b5;
+    @FXML private Button b6;
+    @FXML private Button b7;
+    @FXML private Button b8;
+    @FXML private Button b9;
+    @FXML private Button b10;
+    @FXML private Button b11;
+    @FXML private Button b12;
+    @FXML private Button b13;
+    @FXML private Button b14;
+    @FXML private Button b15;
+    @FXML private Button b16;
+    @FXML private Button b17;
+    @FXML private Button b18;
+    @FXML private Button b19;
+    @FXML private Button b20;
+    @FXML private Button b21;
+    @FXML private Button b22;
+    @FXML private Button b23;
+    @FXML private Button b24;
+    @FXML private Button b25;
+    @FXML private Button b26;
+    @FXML private Button b27;
+    @FXML private Button b28;
+    @FXML private Button b29;
+    @FXML private Button b30;
+    @FXML private Button b31;
+    @FXML private Button b32;
+    @FXML private Button b33;
+    @FXML private Button b34;
+    @FXML private Button b35;
+    @FXML private Button b36;
+
+
+
+    @FXML
+    protected void tableButtonHandler() throws IOException {
+        setText.setOnAction(event -> textField.setText(text));
+    }
+
+    @FXML protected void b1Clicked()  {b1.setVisible(false);}
+    @FXML protected void b2Clicked()  {b2.setVisible(false);}
+    @FXML protected void b3Clicked()  {b2.setVisible(false);}
+    @FXML protected void b4Clicked()  {b4.setVisible(false);}
+    @FXML protected void b5Clicked()  {b5.setVisible(false);}
+    @FXML protected void b6Clicked()  {b6.setVisible(false);}
+    @FXML protected void b7Clicked()  {b7.setVisible(false);}
+    @FXML protected void b8Clicked()  {b8.setVisible(false);}
+    @FXML protected void b9Clicked()  {b9.setVisible(false);}
+    @FXML protected void b10Clicked() {b10.setVisible(false);}
+    @FXML protected void b11Clicked() {b11.setVisible(false);}
+    @FXML protected void b12Clicked() {b12.setVisible(false);}
+    @FXML protected void b13Clicked() {b13.setVisible(false);}
+    @FXML protected void b14Clicked() {b14.setVisible(false);}
+    @FXML protected void b15Clicked() {b15.setVisible(false);}
+    @FXML protected void b16Clicked() {b16.setVisible(false);}
+    @FXML protected void b17Clicked() {b17.setVisible(false);}
+    @FXML protected void b18Clicked() {b18.setVisible(false);}
+    @FXML protected void b19Clicked() {b19.setVisible(false);}
+    @FXML protected void b20Clicked() {b20.setVisible(false);}
+    @FXML protected void b21Clicked() {b21.setVisible(false);}
+    @FXML protected void b22Clicked() {b22.setVisible(false);}
+    @FXML protected void b23Clicked() {b23.setVisible(false);}
+    @FXML protected void b24Clicked() {b24.setVisible(false);}
+    @FXML protected void b25Clicked() {b25.setVisible(false);}
+    @FXML protected void b26Clicked() {b26.setVisible(false);}
+    @FXML protected void b27Clicked() {b27.setVisible(false);}
+    @FXML protected void b28Clicked() {b28.setVisible(false);}
+    @FXML protected void b29Clicked() {b29.setVisible(false);}
+    @FXML protected void b30Clicked() {b30.setVisible(false);}
+    @FXML protected void b31Clicked() {b31.setVisible(false);}
+    @FXML protected void b32Clicked() {b32.setVisible(false);}
+    @FXML protected void b33Clicked() {b33.setVisible(false);}
+    @FXML protected void b34Clicked() {b34.setVisible(false);}
+    @FXML protected void b35Clicked() {b35.setVisible(false);}
+    @FXML protected void b36Clicked() {b36.setVisible(false);}
+}
