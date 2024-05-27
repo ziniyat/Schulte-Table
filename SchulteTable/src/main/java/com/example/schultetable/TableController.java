6c389f2f5418799a9c84a4d0a9a75846ab119eca
koshkilda <89313188+koshkilda@users.noreply.github.com>
2024-05-27 10:36:31 +0300

    Update TableController.java

--- SchulteTable/src/main/java/com/example/schultetable/TableController.java
+++ SchulteTable/src/main/java/com/example/schultetable/TableController.java
@@ -1,20 +1,16 @@
 package com.example.schultetable;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class TableController {
 
-    private String[] buttons;
-
-    @FXML
-    private Button mode4x4;
+    @FXML private Label timeResult;
+    @FXML private Label levelResult;
 
     @FXML private Button b1;
     @FXML private Button b2;
@@ -54,46 +50,118 @@ public class TableController {
     @FXML private Button b36;
 
 
+    protected int  buttonsAmount = 0;
+    protected int  tblLevel      = 0; // 4, 5 or 6
+    protected long startTime     = 0;
+    protected long endTime       = 0;
 
-    @FXML
-    protected void tableButtonHandler() throws IOException {
-        setText.setOnAction(event -> textField.setText(text));
+    final protected int[] nums = {
+            1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+            31, 32, 33, 34, 35, 36
+    };
+
+    protected int[] newRandNums(int length) {
+        int[] randNums = {};
+        if (length > nums.length)
+            return randNums;
+
+        randNums = new int[length];
+
+        System.arraycopy(nums, 0, randNums, 0, randNums.length);
+        mixNums(randNums);
+        return randNums;
     }
 
-    @FXML protected void b1Clicked()  {b1.setVisible(false);}
-    @FXML protected void b2Clicked()  {b2.setVisible(false);}
-    @FXML protected void b3Clicked()  {b2.setVisible(false);}
-    @FXML protected void b4Clicked()  {b4.setVisible(false);}
-    @FXML protected void b5Clicked()  {b5.setVisible(false);}
-    @FXML protected void b6Clicked()  {b6.setVisible(false);}
-    @FXML protected void b7Clicked()  {b7.setVisible(false);}
-    @FXML protected void b8Clicked()  {b8.setVisible(false);}
-    @FXML protected void b9Clicked()  {b9.setVisible(false);}
-    @FXML protected void b10Clicked() {b10.setVisible(false);}
-    @FXML protected void b11Clicked() {b11.setVisible(false);}
-    @FXML protected void b12Clicked() {b12.setVisible(false);}
-    @FXML protected void b13Clicked() {b13.setVisible(false);}
-    @FXML protected void b14Clicked() {b14.setVisible(false);}
-    @FXML protected void b15Clicked() {b15.setVisible(false);}
-    @FXML protected void b16Clicked() {b16.setVisible(false);}
-    @FXML protected void b17Clicked() {b17.setVisible(false);}
-    @FXML protected void b18Clicked() {b18.setVisible(false);}
-    @FXML protected void b19Clicked() {b19.setVisible(false);}
-    @FXML protected void b20Clicked() {b20.setVisible(false);}
-    @FXML protected void b21Clicked() {b21.setVisible(false);}
-    @FXML protected void b22Clicked() {b22.setVisible(false);}
-    @FXML protected void b23Clicked() {b23.setVisible(false);}
-    @FXML protected void b24Clicked() {b24.setVisible(false);}
-    @FXML protected void b25Clicked() {b25.setVisible(false);}
-    @FXML protected void b26Clicked() {b26.setVisible(false);}
-    @FXML protected void b27Clicked() {b27.setVisible(false);}
-    @FXML protected void b28Clicked() {b28.setVisible(false);}
-    @FXML protected void b29Clicked() {b29.setVisible(false);}
-    @FXML protected void b30Clicked() {b30.setVisible(false);}
-    @FXML protected void b31Clicked() {b31.setVisible(false);}
-    @FXML protected void b32Clicked() {b32.setVisible(false);}
-    @FXML protected void b33Clicked() {b33.setVisible(false);}
-    @FXML protected void b34Clicked() {b34.setVisible(false);}
-    @FXML protected void b35Clicked() {b35.setVisible(false);}
-    @FXML protected void b36Clicked() {b36.setVisible(false);}
+    protected static void mixNums(int[] arr) {
+        Random rnd = new Random();
+
+        for (int i = 0; i < arr.length; i++) {
+            int index = rnd.nextInt(i + 1);
+            int temp = arr[index];
+            arr[index] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+
+    @FXML protected void onStart4x4() { if (tblLevel == 0) initTable(4); }
+    @FXML protected void onStart5x5() { if (tblLevel == 0) initTable(5); }
+    @FXML protected void onStart6x6() { if (tblLevel == 0) initTable(6); }
+
+
+    protected void initTable(int level) {
+        tblLevel = level;
+        buttonsAmount = tblLevel * tblLevel;
+
+        Button[] buttons = new Button[]{
+                b1,  b2,  b3,  b4,  b5,  b6,  b7,  b8,  b9,
+                b10, b11, b12, b13, b14, b15, b16, b17, b18,
+                b19, b20, b21, b22, b23, b24, b25, b26, b27,
+                b28, b29, b30, b31, b32, b33, b34, b35, b36
+        };
+
+        int[] buttonNums = newRandNums(buttonsAmount);
+
+        for (int i = 0; i < buttonNums.length; i++) {
+            buttons[i].setText(Integer.toString(buttonNums[i]));
+        }
+        startTime = System.currentTimeMillis();
+    }
+
+    @FXML
+    protected void onEnd() throws IOException {
+        if (buttonsAmount == 0 && endTime == 0) {
+            endTime = System.currentTimeMillis();
+            long timeElapsed = endTime - startTime;
+
+            timeResult.setText("Elapsed time: " + timeElapsed / 1000 + "s");
+            levelResult.setText("Level: " + tblLevel + "x" + tblLevel);
+        }
+    }
+
+    protected void buttonHandler(Button button) {
+        if (button.getText().equals(Integer.toString(buttonsAmount))) {
+            button.setVisible(false);
+            buttonsAmount -= 1;
+        }
+    }
+
+    @FXML protected void b1Clicked()  {buttonHandler(b1);}
+    @FXML protected void b2Clicked()  {buttonHandler(b2);}
+    @FXML protected void b3Clicked()  {buttonHandler(b3);}
+    @FXML protected void b4Clicked()  {buttonHandler(b4);}
+    @FXML protected void b5Clicked()  {buttonHandler(b5);}
+    @FXML protected void b6Clicked()  {buttonHandler(b6);}
+    @FXML protected void b7Clicked()  {buttonHandler(b7);}
+    @FXML protected void b8Clicked()  {buttonHandler(b8);}
+    @FXML protected void b9Clicked()  {buttonHandler(b9);}
+    @FXML protected void b10Clicked()  {buttonHandler(b10);}
+    @FXML protected void b11Clicked()  {buttonHandler(b11);}
+    @FXML protected void b12Clicked()  {buttonHandler(b12);}
+    @FXML protected void b13Clicked()  {buttonHandler(b13);}
+    @FXML protected void b14Clicked()  {buttonHandler(b14);}
+    @FXML protected void b15Clicked()  {buttonHandler(b15);}
+    @FXML protected void b16Clicked()  {buttonHandler(b16);}
+    @FXML protected void b17Clicked()  {buttonHandler(b17);}
+    @FXML protected void b18Clicked()  {buttonHandler(b18);}
+    @FXML protected void b19Clicked()  {buttonHandler(b19);}
+    @FXML protected void b20Clicked()  {buttonHandler(b20);}
+    @FXML protected void b21Clicked()  {buttonHandler(b21);}
+    @FXML protected void b22Clicked()  {buttonHandler(b22);}
+    @FXML protected void b23Clicked()  {buttonHandler(b23);}
+    @FXML protected void b24Clicked()  {buttonHandler(b24);}
+    @FXML protected void b25Clicked()  {buttonHandler(b25);}
+    @FXML protected void b26Clicked()  {buttonHandler(b26);}
+    @FXML protected void b27Clicked()  {buttonHandler(b27);}
+    @FXML protected void b28Clicked()  {buttonHandler(b28);}
+    @FXML protected void b29Clicked()  {buttonHandler(b29);}
+    @FXML protected void b30Clicked()  {buttonHandler(b30);}
+    @FXML protected void b31Clicked()  {buttonHandler(b31);}
+    @FXML protected void b32Clicked()  {buttonHandler(b32);}
+    @FXML protected void b33Clicked()  {buttonHandler(b33);}
+    @FXML protected void b34Clicked()  {buttonHandler(b34);}
+    @FXML protected void b35Clicked()  {buttonHandler(b35);}
+    @FXML protected void b36Clicked()  {buttonHandler(b36);}
 }
